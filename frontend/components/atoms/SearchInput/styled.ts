49c58628401ed37403ffcaca3205e62299924c38
DevJo <sa02045@naver.com>
2021-11-02 20:10:44 +0900

    :recycle: refactor: 검색바 input 분리

---
+++ frontend/components/atoms/SearchInput/styled.ts
@@ -0,0 +1,12 @@
+import styled from "styled-components";
+
+const StyledSearchInput = styled.input`
+  border: none;
+  border-bottom: 2px solid #f48024;
+  :focus {
+    outline: none;
+  }
+  margin-right: 10px;
+`;
+
+export { StyledSearchInput };
