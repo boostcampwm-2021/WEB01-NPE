d0dbc78f5702166f069316fdf493a852225f8961
getState <asj446@naver.com>
2021-11-02 19:19:53 +0900

    :pencil2: feat: [FE] SideTag Storybook 테스트 코드 작성
    - Default와 길이가 긴 Long태그, 색이 다른 태그 작성

---
+++ frontend/components/atoms/SideTag/sideTag.stories.tsx
@@ -0,0 +1,37 @@
+import React, { useState } from 'react';
+import SideTag from "./index";
+
+
+export default{
+    title: 'Atoms/SideTag',
+    component: SideTag
+}
+
+
+export const Default = () => {
+    return (
+        <SideTag 
+            text = "react.js"
+            deleteHadler= {() => {}}
+        />
+    );
+}
+
+export const LongTag = () => {
+    return (
+        <SideTag 
+            text = "javascript webpack"
+            deleteHadler= {() => {}}
+        />
+    );
+}
+
+export const otherColorTag = () => {
+    return (
+        <SideTag 
+            text = "javascript"
+            tagBgColor="#045D8B"
+            deleteHadler= {() => {}}
+        />
+    );
+}
