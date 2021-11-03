04be3e966a8fc02add9577ad5a7f2aceb02b14c5
getState <asj446@naver.com>
2021-11-03 21:57:35 +0900

    :sparkles: feat: [FE] TagSearch 리액트 컴포넌트 구현
    - 후보 태그들은 사용자 입력이 변함에 따라 드롭다운으로 조건부 렌더링
    - onSubmit으로 사용자가 추가했을 때의 함수를 props로 받도록 설정

---
+++ frontend/src/components/molecules/TagSearch/index.tsx
@@ -0,0 +1,53 @@
+import React, { FunctionComponent, useRef, useState } from "react";
+import * as Styled from "./styled";
+
+interface Props{
+  onSubmit: VoidFunction;
+  tagList: string[]
+}
+
+const TagSearch: FunctionComponent<Props> = ({onSubmit, tagList}) => {
+  const [candidateTags, setTags] = useState<string[]>([]);
+  const inputTag = useRef<HTMLInputElement>(null);
+  const onTagClick = (event: React.ChangeEvent<HTMLInputElement>) => {
+    const tagName = event.target.dataset.tag;
+    inputTag.current.value = tagName;
+    setTags([]);
+  }
+  const onInput = (event: React.ChangeEvent<HTMLInputElement>) => {
+    const {value} = event.target;
+    if(value.length === 0){
+      return setTags([]);
+    }
+    const tags:string[] = tagList.filter((tag:string) => tag.startsWith(value));
+    setTags(tags);
+  }
+  const getItem = (tag: string, index:number) => {
+    return (
+      <Styled.Tag key={index} data-tag={tag} onClick={onTagClick}>
+          {tag}
+      </Styled.Tag>
+    );
+  }
+  return (
+    <Styled.Container>
+      <Styled.Input 
+        type="text"
+        placeholder="tag를 입력하세요"
+        onInput={onInput}
+        ref={inputTag}
+      />
+      <Styled.Button
+        onClick={onSubmit}
+      >추가
+      </Styled.Button>
+      {candidateTags.length !== 0 && <Styled.TagList>
+        {candidateTags.map(getItem)}  
+        </Styled.TagList>}
+    </Styled.Container>
+
+
+  );
+}
+
+export default TagSearch;
