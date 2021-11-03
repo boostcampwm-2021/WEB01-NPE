6799a56e4bc1e9b7694aceb066ac2318c518c90c
hwangwoojin <hwangwoojin.dev@gmail.com>
2021-11-03 11:22:27 +0900

    :recycle: refactor: [FE] molecule 리팩토링
    기존 ProfileImage 를 사용하는 컴포넌트를 Image 로 리팩토링

--- frontend/components/molecules/ProfileHeader/index.tsx
+++ frontend/components/molecules/ProfileHeader/index.tsx
@@ -1,7 +1,7 @@
 import React, { FunctionComponent } from "react"
 import { Button, ImageBox, TextBox } from './styled'
-import ProfileImage from '../../atoms/ProfileImage'
-import SubTitleText from '../../atoms/Text'
+import Image from '../../atoms/Image'
+import Text from '../../atoms/Text'
 
 interface Props {
     src: string,
@@ -13,10 +13,10 @@ const ProfileHeader: FunctionComponent<Props> = ({ src, text, onClick }) => {
     return (
         <Button onClick={() => onClick()}>
             <ImageBox size={24}>
-                <ProfileImage src={src} size={24} />
+                <Image src={src} width={24} height={24} />
             </ImageBox>
             <TextBox>
-                <SubTitleText text={text} ellipsis={true} fontSize={12} fontWeight={'bold'} />
+                <Text text={text} ellipsis={true} fontSize={12} fontWeight={'bold'} />
             </TextBox>
         </Button>
     );
