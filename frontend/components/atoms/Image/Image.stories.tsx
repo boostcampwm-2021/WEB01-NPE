b9b91eb1d2ea8c04769db80f2a391f5e65470781
hwangwoojin <hwangwoojin.dev@gmail.com>
2021-11-03 11:21:02 +0900

    :recycle: refactor: [FE] Image 아톰 컴포넌트 수정
    기존 ProfileImage 를 커스텀 로더를 사용하는 Image 컴포넌트로 리팩토링

---
+++ frontend/components/atoms/Image/Image.stories.tsx
@@ -0,0 +1,18 @@
+import Image from './index'
+
+export default {
+    Component: Image,
+    title: 'Atoms/Image'
+}
+
+export const normal = () => {
+    return (
+        <Image src={'https://avatars.githubusercontent.com/u/50866506?v=4'} width={24} height={24} />
+    );
+};
+
+export const large = () => {
+    return (
+        <Image src={'https://avatars.githubusercontent.com/u/50866506?v=4'} width={48} height={48}/>
+    );
+};
