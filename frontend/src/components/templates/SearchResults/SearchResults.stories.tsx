048da5077be646234eb75f1107aa5613339deb3d
DevJo <sa02045@naver.com>
2021-11-04 09:13:11 +0900

    :lipstick: Feat: Template-단일검색결과 리스트 UI추가

---
+++ frontend/src/components/templates/SearchResults/SearchResults.stories.tsx
@@ -0,0 +1,10 @@
+import SearchResults from ".";
+
+export default {
+  Component: SearchResults,
+  title: "Templates/SearchResults",
+};
+
+export const example = () => {
+  return <SearchResults />;
+};
