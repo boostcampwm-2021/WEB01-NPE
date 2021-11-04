546606f1a8869245ca219530d199e0f21f910dfa
getState <asj446@naver.com>
2021-11-04 16:42:30 +0900

    :zap: feat: [FE] SideBar 리액트 컴포넌트 구현

---
+++ frontend/src/components/organisms/SideBar/index.tsx
@@ -0,0 +1,48 @@
+import React, { FunctionComponent, useState } from "react";
+import { SideTag, Switch } from "../../atoms";
+import TitleText from "../../atoms/TitleText";
+import { TagSearch } from "../../molecules";
+import * as Styled from "./styled";
+
+interface Props {
+  selectedTags: string[];
+  setSelectedTags: (value: string[]) => void;
+}
+
+const tagList = ["react.js", "javascript", "react-router-dom"];
+
+const SideBar: FunctionComponent<Props> = ({
+  selectedTags,
+  setSelectedTags,
+}) => {
+  const [isLive, setIsLive] = useState<boolean>(false);
+  const onSubmit = (newTag: string) => {
+    setSelectedTags([...selectedTags, newTag]);
+  };
+  const onDelete = (deletedTag: string): void => {
+    setSelectedTags((preTags: string[]): string[] =>
+      preTags.filter((preTag: string) => preTag !== deletedTag)
+    );
+  };
+  return (
+    <Styled.Container>
+      <TagSearch tagList={tagList} onSubmit={onSubmit} />
+      <Styled.LiveContainer>
+        <TitleText text="실시간 답변" />
+        <Switch
+          isChecked={isLive}
+          setIsChecked={setIsLive}
+          offColor="#66bb6a"
+          onColor="#ccc"
+        />
+      </Styled.LiveContainer>
+      <Styled.UlTags>
+        {selectedTags.map((tag) => (
+          <SideTag text={tag} onDelete={onDelete} />
+        ))}
+      </Styled.UlTags>
+    </Styled.Container>
+  );
+};
+
+export default SideBar;
