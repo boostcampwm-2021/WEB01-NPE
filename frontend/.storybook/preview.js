ea589013312fc99c288c4d926446e31576713a56
hwangwoojin <hwangwoojin.dev@gmail.com>
2021-11-04 16:32:57 +0900

    :wrench: feat: [FE] storybook 이미지 설정
    next/image 를 storybook 에서도 사용할 수 있도록 설정

--- frontend/.storybook/preview.js
+++ frontend/.storybook/preview.js
@@ -1,5 +1,17 @@
+import * as nextImage from 'next/image';
+
+Object.defineProperty(nextImage, 'default', {
+  configurable: true,
+  value: props => < img {
+    ...props
+  }
+  />
+});
+
 export const parameters = {
-  actions: { argTypesRegex: "^on[A-Z].*" },
+  actions: {
+    argTypesRegex: "^on[A-Z].*"
+  },
   controls: {
     matchers: {
       color: /(background|color)$/i,
