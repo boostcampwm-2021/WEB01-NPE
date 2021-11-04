d037997fbbe8c1994ecc445468c36aa694ce7556
hwangwoojin <hwangwoojin.dev@gmail.com>
2021-11-04 16:40:24 +0900

    :sparkles: feat: [FE] 헤더 구현
    헤더 구현, 이벤트는 클릭시 드롭다운 내려오는 것만 구현

---
+++ frontend/src/components/organisms/Header/index.tsx
@@ -0,0 +1,64 @@
+import React, { FunctionComponent, useState } from "react";
+import * as Styled from "./styled";
+import * as Atoms from "../../atoms";
+import * as Molecules from "../../molecules";
+import userImg from "./user.png";
+import questionImg from "./question.png";
+import logoutImg from "./logout.png";
+
+interface Props {
+  userName?: string;
+  userImage?: string;
+}
+
+const Header: FunctionComponent<Props> = ({ userName, userImage }) => {
+  const [isDropdown, setIsDropdown] = useState(false);
+  const onDropdown = () => {
+    setIsDropdown((props) => !props);
+  };
+  return (
+    <Styled.HeaderDiv>
+      <Styled.LogoAnchor href="">
+        <Atoms.LogoFull />
+      </Styled.LogoAnchor>
+      <Styled.SearchDiv>
+        <Atoms.SearchInput placeholder={"Search..."} width={"643px"} />
+      </Styled.SearchDiv>
+      <Styled.ButtonDiv>
+        {userName ? (
+          <div>
+            <Molecules.ProfileHeader
+              src={userImage!}
+              text={userName!}
+              onClick={onDropdown}
+            />
+            {isDropdown ? (
+              <Styled.DropdownDiv>
+                <Molecules.ProfileDropdown />
+              </Styled.DropdownDiv>
+            ) : (
+              <></>
+            )}
+          </div>
+        ) : (
+          <Atoms.Button
+            image={userImg}
+            text={"로그인"}
+            bgColor="#F48024"
+            textColor="white"
+            onClick={() => {}}
+          />
+        )}
+        <Atoms.Button
+          image={questionImg}
+          text={"질문하기"}
+          bgColor="#F48024"
+          textColor="white"
+          onClick={() => {}}
+        />
+      </Styled.ButtonDiv>
+    </Styled.HeaderDiv>
+  );
+};
+
+export default Header;
