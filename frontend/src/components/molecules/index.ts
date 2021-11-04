26d7a52e3c15c2aa6da8b29756d0bd814504cedd
DevJo <sa02045@naver.com>
2021-11-04 09:22:28 +0900

    :recycle: refactor: export import as index.ts

---
+++ frontend/src/components/molecules/index.ts
@@ -0,0 +1,4 @@
+export { default as ProfileDropdown } from "./profile-dropdown";
+export { default as ProfileHeader } from "./ProfileHeader";
+export { default as ProfileSummary } from "./ProfileSummary";
+export { default as TagSearch } from "./TagSearch";
