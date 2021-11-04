c5f8db8c72515d32529a23653645eb58c405f02e
getState <asj446@naver.com>
2021-11-04 22:27:36 +0900

    :construction: fix: [FE] SideBar 컴포넌트 스타일 변경

--- frontend/src/components/organisms/SideBar/styled.ts
+++ frontend/src/components/organisms/SideBar/styled.ts
@@ -1,10 +1,16 @@
 import styled from "styled-components";
 
 export const Container = styled.aside`
+  align-self: start;
+  position: sticky;
+  top: 120px;
+  z-index: 10;
   width: 200px;
   box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.1), 0px 4px 20px rgba(0, 0, 0, 0.1);
+  border-radius: 20px;
   margin-top: 4px;
-  padding: 10px;
+  min-height: 250px;
+  padding: 20px 10px 10px 10px;
 `;
 
 export const UlTags = styled.ul`
@@ -17,7 +23,7 @@ export const LiveContainer = styled.div`
   justify-content: space-between;
   align-items: center;
   margin-top: 15px;
-  padding-right: 15px;
+  padding: 0px 10px;
   & > h2 {
     font-size: 15px;
   }
