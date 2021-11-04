9564b9d9723624e4eedec9525d08ac31a6b9b450
getState <asj446@naver.com>
2021-11-04 22:28:03 +0900

    :construction: fix: [FE] SearchResults 스타일 및 컴포넌트 변경

--- frontend/src/components/templates/SearchResults/styled.ts
+++ frontend/src/components/templates/SearchResults/styled.ts
@@ -1,6 +1,9 @@
 import styled from "styled-components";
 
 export const StyledSearchResults = styled.div`
+  flex-grow: 1;
   display: flex;
   flex-direction: column;
+  align-items: center;
+  padding: 30px;
 `;
