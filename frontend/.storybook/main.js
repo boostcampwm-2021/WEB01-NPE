12c98407a763e5c2a5c5f307bb11f3188c81fbd2
getState <asj446@naver.com>
2021-11-04 22:23:58 +0900

    :ambulance: fix: [FE] storybook 경로 수정

--- frontend/.storybook/main.js
+++ frontend/.storybook/main.js
@@ -1,9 +1,4 @@
 module.exports = {
-  "stories": [
-    '../src/components/**/*.stories.tsx'
-  ],
-  "addons": [
-    "@storybook/addon-links",
-    "@storybook/addon-essentials"
-  ]
-}
+  stories: ["../src/**/*.stories.tsx"],
+  addons: ["@storybook/addon-links", "@storybook/addon-essentials"],
+};
