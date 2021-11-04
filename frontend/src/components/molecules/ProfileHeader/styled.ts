557f55926c7aba8fb129a51afd38dd85e25a39d6
hwangwoojin <hwangwoojin.dev@gmail.com>
2021-11-04 16:36:07 +0900

    :sparkles: feat: [FE] profileHeader 수정
    기존 a 태그를 div 로 바꾸고 onClick 이벤트 달아줌

--- frontend/src/components/molecules/ProfileHeader/styled.ts
+++ frontend/src/components/molecules/ProfileHeader/styled.ts
@@ -1,6 +1,6 @@
 import styled from "styled-components";
 
-export const Anchor = styled.a`
+export const ProfileDiv = styled.div`
   display: flex;
   justify-content: space-between;
   align-items: center;
