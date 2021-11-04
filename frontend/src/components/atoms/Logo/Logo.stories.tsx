3b01fc3a107181f39857b54583dcf53e6e43b46f
hwangwoojin <hwangwoojin.dev@gmail.com>
2021-11-04 16:37:48 +0900

    :sparkles: feat: [FE] Logo 아톰 컴포넌트 추가
    단일 로고 이미지 컴포넌트 추가

---
+++ frontend/src/components/atoms/Logo/Logo.stories.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+import Logo from "./index";
+
+export default {
+  title: "Atoms/Logo",
+  component: Logo,
+};
+
+export const Default = () => {
+  return (
+    <>
+      <Logo />
+    </>
+  );
+};
---
+++ frontend/src/components/atoms/Logo/index.tsx
@@ -0,0 +1,11 @@
+import React, { FunctionComponent } from "react";
+import Image from "next/image";
+import LogoImage from "./logo.png";
+
+interface Props {}
+
+const Logo: FunctionComponent<Props> = ({}) => {
+  return <Image src={LogoImage} width={48} height={48} alt=".."></Image>;
+};
+
+export default Logo;
