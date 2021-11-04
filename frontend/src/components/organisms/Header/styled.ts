7fa55659715242ca74c1a5a615e5ea03b018c0c8
getState <asj446@naver.com>
2021-11-04 22:26:28 +0900

    :construction: fix: [FE] 헤더 스타일 변경

--- frontend/src/components/organisms/Header/styled.ts
+++ frontend/src/components/organisms/Header/styled.ts
@@ -1,20 +1,31 @@
 import styled from "styled-components";
 
-export const HeaderDiv = styled.div`
+export const HeaderDiv = styled.header`
+  position: sticky;
+  top: 0px;
+  z-index: 100;
+  width: 100%;
+  max-width: 1200px;
+  margin-left: auto;
+  margin-right: auto;
   display: flex;
   justify-content: space-between;
   align-items: center;
   height: 70px;
+  background-color: white;
   box-shadow: 0 5px 15px -10px rgba(0, 0, 0, 0.3);
 `;
 
 export const LogoAnchor = styled.a``;
 
 export const SearchDiv = styled.div`
+  flex-grow: 1;
+  min-width: 250px;
   display: flex;
   justify-content: center;
   width: 643px;
   margin-top: 16px;
+  padding: 0px 30px;
 `;
 
 export const ButtonDiv = styled.div`
