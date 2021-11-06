c359a3bdf32c16f6f9784e730c97e1e192ce382e
hwangwoojin <hwangwoojin.dev@gmail.com>
2021-11-06 22:14:36 +0900

    :recycle: refactor: [FE] Logo 컴포넌트 리팩토링
    message props 로 short, long 로고 보여주도록 구현

--- frontend/src/components/atoms/Logo/index.tsx
+++ frontend/src/components/atoms/Logo/index.tsx
@@ -1,11 +1,29 @@
 import React, { FunctionComponent } from "react";
 import Image from "next/image";
-import LogoImage from "./logo.png";
+import logoImage from "./logo.png";
+import fullLogoImage from "./fullLogo.png";
 
-interface Props {}
+interface Props {
+  message: string;
+}
 
-const Logo: FunctionComponent<Props> = ({}) => {
-  return <Image src={LogoImage} width={48} height={48} alt=".."></Image>;
+const Logo: FunctionComponent<Props> = ({ message }) => {
+  return (
+    <>
+      {message === "short" && (
+        <Image src={logoImage} width={48} height={48} alt="logo"></Image>
+      )}
+
+      {message === "long" && (
+        <Image
+          src={fullLogoImage}
+          width={321}
+          height={48}
+          alt="full logo"
+        ></Image>
+      )}
+    </>
+  );
 };
 
 export default Logo;
