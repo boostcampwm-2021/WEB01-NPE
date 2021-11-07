c00347f252295fb4ada55f377f81f6e55d97535e
DevJo <sa02045@naver.com>
2021-11-08 08:11:03 +0900

    :recycle: refactor: Atom Indicator

--- frontend/src/components/atoms/Indicator/index.tsx
+++ frontend/src/components/atoms/Indicator/index.tsx
@@ -1,32 +1,21 @@
 import React, { FunctionComponent } from "react";
-import { StyledIndicator } from "./styled";
+import * as Styled from "./styled";
 
 interface Props {
   isOnline: boolean;
-  width?: string;
-  height?: string;
 }
 
-const Indicator: FunctionComponent<Props> = ({ isOnline, width, height }) => {
-  if (isOnline) {
-    return (
-      <StyledIndicator
-        title="현재 라이브 답변을 대기중에 있습니다."
-        bgColor="#6cc16f"
-        width={width || "16px"}
-        height={height || "16px"}
-      />
-    );
-  } else {
-    return (
-      <StyledIndicator
-        title="현재 질문자가 오프라인 상태입니다."
-        bgColor="#e26a61"
-        width={width || "16px"}
-        height={height || "16px"}
-      />
-    );
-  }
+const Indicator: FunctionComponent<Props> = ({ isOnline }) => {
+  return (
+    <Styled.Indicator
+      title={
+        isOnline
+          ? "현재 라이브 답변을 대기중에 있습니다."
+          : "현재 질문자가 오프라인 상태입니다."
+      }
+      bgColor={isOnline ? "#6cc16f" : "#e26a61"}
+    />
+  );
 };
 
 export default Indicator;
