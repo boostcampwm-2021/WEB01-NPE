4f14fee2bc9251fbf447e4f2daca17511f01b14a
getState <asj446@naver.com>
2021-11-08 18:43:44 +0900

    :wrench: fix: [FE] molecules index.js 수정
    ProfileDropdown 폴더명 수정에 따른 index.js 수정

--- frontend/src/components/molecules/index.ts
+++ frontend/src/components/molecules/index.ts
@@ -1,4 +1,4 @@
-export { default as ProfileDropdown } from "./profile-dropdown";
+export { default as ProfileDropdown } from "./ProfileDropdown";
 export { default as ProfileHeader } from "./ProfileHeader";
 export { default as ProfileSummary } from "./ProfileSummary";
 export { default as TagSearch } from "./TagSearch";
