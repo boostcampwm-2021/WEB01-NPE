7ba418e41872dabc4f60a14ce0f83b4a80a57606
getState <asj446@naver.com>
2021-11-08 18:40:48 +0900

    :wrench: fix: [FE] Switch 컴포넌트 리팩토링

--- frontend/src/components/atoms/Switch/styled.ts
+++ frontend/src/components/atoms/Switch/styled.ts
@@ -1,44 +1,44 @@
 import styled from "styled-components";
 interface SliderProps {
-  checked: boolean;
   offColor: string;
   onColor: string;
 }
-const Container = styled.label`
-  display: block;
-  height: 34px;
-  position: relative;
-  width: 60px;
-`;
+export const Container = styled.label``;
 
-const Input = styled.input`
-  display: none;
-`;
-
-const Slider = styled.div<SliderProps>`
-  background-color: ${(props) =>
-    props.checked ? props.offColor : props.onColor};
-  bottom: 0;
-  cursor: pointer;
-  border-radius: 34px;
-  left: 0;
+export const Input = styled.input`
+  opacity: 0;
   position: absolute;
-  right: 0;
-  top: 0;
-  transition: 0.4s;
-  &:before {
-    background-color: #fff;
-    bottom: 4px;
-    content: "";
-    height: 26px;
-    left: 4px;
-    position: absolute;
-    transition: 0.4s;
-    width: 26px;
-    border-radius: 50%;
-    transform: ${(props) => props.checked && "translateX(26px)"};
-  }
-  background-color: ;
 `;
 
-export { Container, Input, Slider };
+export const Slider = styled.div<SliderProps>`
+  background-color: ${(props) => props.offColor};
+  border-radius: 50px;
+  cursor: pointer;
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  padding: 5px;
+  position: relative;
+  height: 26px;
+  width: 50px;
+  transform: scale(1.5);
+  transition: background-color 0.4s ease-in-out;
+  ${Input}:checked + & {
+    background-color: ${(props) => props.onColor};
+  }
+`;
+
+export const Ball = styled.div`
+  background-color: #fff;
+  border-radius: 50%;
+  position: absolute;
+  top: 2px;
+  left: 2px;
+  height: 22px;
+  width: 22px;
+  transform: translateX(0px);
+  transition: transform 0.4s ease-in-out;
+  ${Input}:checked + ${Slider} & {
+    transform: translateX(24px);
+  }
+`;
