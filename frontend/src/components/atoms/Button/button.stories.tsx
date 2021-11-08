fbf390af38dad57954698b6d2bbb276b45f8ca5c
getState <asj446@naver.com>
2021-11-08 18:32:14 +0900

    :wrench: fix: [FE] Button 컴포넌트 리팩토링

--- frontend/src/components/atoms/Button/button.stories.tsx
+++ frontend/src/components/atoms/Button/button.stories.tsx
@@ -9,59 +9,20 @@ export default {
 };
 
 export const Default = () => {
-  const [text, setText] = useState("로그인");
-  const onClick = () => {
-    setText((text) => (text === "로그인" ? "프로필" : "로그인"));
-  };
-  return (
-    <Button
-      image={userImg}
-      text={text}
-      bgColor="#F48024"
-      textColor="white"
-      onClick={onClick}
-    />
-  );
-};
-
-export const Logout = () => {
   const [text, setText] = useState("로그아웃");
   const onClick = () => {
     setText((text) => (text === "로그아웃" ? "프로필" : "로그아웃"));
   };
 
   return (
-    <Button
-      image={logoutImg}
-      text={text}
-      bgColor="#fff"
-      textColor="black"
-      onClick={onClick}
-    />
+    <Button type="Default" image={logoutImg} text={text} onClick={onClick} />
   );
 };
 
-export const OnlyTextButton = () => {
-  return (
-    <Button
-      text="로그인"
-      bgColor="#F48024"
-      textColor="white"
-      onClick={() => {}}
-    />
-  );
-};
-
-export const LongButton = () => {
-  const [text, setText] = useState("라이브스트리밍onoff");
-
-  return (
-    <Button
-      text={text}
-      bgColor="#fff"
-      textColor="black"
-      onClick={() => {}}
-      width="200px"
-    />
-  );
+export const Header = () => {
+  const [text, setText] = useState("로그인");
+  const onClick = () => {
+    setText((text) => (text === "로그인" ? "프로필" : "로그인"));
+  };
+  return <Button type="Header" image={userImg} text={text} onClick={onClick} />;
 };
