8a2b439b1fa95c9852f3b94d1b9530dbdcc7115a
hwangwoojin <hwangwoojin.dev@gmail.com>
2021-11-10 08:39:20 +0900

    :recycle: refactor: [FE] 로그인 모달 리팩토링
    onclick 을 props 로 받지 않도록 구현

--- frontend/src/components/molecules/LoginModal/index.tsx
+++ frontend/src/components/molecules/LoginModal/index.tsx
@@ -1,11 +1,14 @@
 import React, { FunctionComponent } from "react";
+import { signIn } from "next-auth/client";
 import * as Styled from "./styled";
 import * as Atoms from "../../atoms";
 import Github from "./github.png";
 
-const LoginModal: FunctionComponent<{ onClick: () => void }> = ({
-  onClick,
-}) => {
+const LoginModal: FunctionComponent = () => {
+  const signInGithub = () => {
+    signIn("github", { redirect: false });
+  };
+
   return (
     <Styled.Div>
       <Atoms.Logo type="Short" />
@@ -15,7 +18,7 @@ const LoginModal: FunctionComponent<{ onClick: () => void }> = ({
         type="Github"
         image={Github}
         text="GitHub 으로 로그인"
-        onClick={onClick}
+        onClick={signInGithub}
       />
       <Atoms.Text
         type="Header"
