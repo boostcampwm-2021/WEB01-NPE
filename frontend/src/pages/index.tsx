6fc809cf86979935ea58160905b14f0323a4eba4
DevJo <sa02045@naver.com>
2021-11-09 23:31:50 +0900

    :recycle: refactor: pages 폴더 리팩토링
    api폴더 제거, MainPage 폴더제거, index.tsx 생성

--- frontend/src/pages/index.tsx
+++ frontend/src/pages/index.tsx
@@ -1,11 +1,11 @@
 import type { NextPage } from "next";
-import Head from "next/head";
-import Image from "next/image";
-import styles from "../styles/Home.module.css";
-import MainPage from "./MainPage";
 
 const Home: NextPage = () => {
-  return <MainPage />;
+  return (
+    <>
+      <div>메인페이지</div>
+    </>
+  );
 };
 
 export default Home;
