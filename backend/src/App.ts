18f83532b4d2f03b0615c3b34203077d0d61aeae
jangdawit <jangdawit@gmail.com>
2021-11-10 18:44:33 +0900

    🏗️ Chore: 환경 변수 설정 (production/test)

--- backend/src/App.ts
+++ backend/src/App.ts
@@ -2,15 +2,27 @@ import "reflect-metadata";
 import express from "express";
 import GraphQLMiddleware from "./graphql";
 import { createConnection } from "typeorm";
+const DB_CONN_INFO = require("../ormconfig.json");
+let env = null;
+if (process.env.NODE_ENV === "production") {
+  env = "production";
+} else {
+  env = "test";
+}
 
 (async () => {
   const app = express();
   // DB 커넥션 생성
-  createConnection();
+  createConnection(DB_CONN_INFO[env]);
 
   const gqMiddleware = await GraphQLMiddleware.get();
 
   app.use("/graphql", gqMiddleware);
 
-  app.listen(3000, () => console.log("server is ON at 3000"));
+  if (process.env.NODE_ENV === "production") {
+    console.info("!!! THIS IS PRODUCTION MODE !!!");
+    app.listen(3000, () => console.log("server is ON at 4000(PRODUCTION)"));
+  } else {
+    app.listen(3000, () => console.log("server is ON at 3000(TEST/DEV)"));
+  }
 })();
