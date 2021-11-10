f3a3fe435cf1abf97390610a1e2328a9f1524558
getState <asj446@naver.com>
2021-11-10 19:28:07 +0900

    :truck: feat: [FE] 마크다운 Viewer 구현

---
+++ frontend/src/components/atoms/MDViewer/MDViewer.stories.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+import MDViewer from ".";
+
+export default {
+  Component: MDViewer,
+  title: "Atoms/MDViewer",
+};
+
+export const Default = () => {
+  return <MDViewer />;
+};
