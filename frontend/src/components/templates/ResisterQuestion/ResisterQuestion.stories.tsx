ae81896dd71bcc93082d1397fc6fa6f0b76defff
getState <asj446@naver.com>
2021-11-10 19:32:45 +0900

    :building_construction: feat: [FE] 질문 등록 Template 구현

---
+++ frontend/src/components/templates/ResisterQuestion/ResisterQuestion.stories.tsx
@@ -0,0 +1,10 @@
+import ResisterQuestion from ".";
+
+export default {
+  Component: ResisterQuestion,
+  title: "Templates/ResisterQuestion",
+};
+
+export const Default = () => {
+  return <ResisterQuestion />;
+};
