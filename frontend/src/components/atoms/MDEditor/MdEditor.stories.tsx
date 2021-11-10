8f20d524a2075fdfa8a39d73c9f4cf2b58c96c04
getState <asj446@naver.com>
2021-11-10 19:27:04 +0900

    :truck: feat: [FE] 마크다운 Editor 컴포넌트 구현

---
+++ frontend/src/components/atoms/MDEditor/MdEditor.stories.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+import MDEditor from ".";
+
+export default {
+  Component: MDEditor,
+  title: "Atoms/MDEditor",
+};
+
+export const Default = () => {
+  return <MDEditor />;
+};
