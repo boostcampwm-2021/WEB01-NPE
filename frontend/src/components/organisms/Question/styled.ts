00670d110dc5eeb1cd36d6b8faec05b78be87a71
getState <asj446@naver.com>
2021-11-11 23:35:45 +0900

    :zap: fix: [FE] Question 컴포넌트 UI수정

--- frontend/src/components/organisms/Question/styled.ts
+++ frontend/src/components/organisms/Question/styled.ts
@@ -3,13 +3,39 @@ import styled from "styled-components";
 export const Question = styled.section`
   display: flex;
   width: 600px;
-  height: 200px;
-  border: 1px solid black;
+  // height: 200px;
+  // border: 1px solid black;
+  margin-bottom: 40px;
+  h2 {
+    margin: 0;
+    font-size: 18px;
+    color: #586069;
+  }
 `;
 
-export const LeftContainer = styled.div``;
-export const RightContainer = styled.div``;
-
+export const LeftContainer = styled.div`
+  margin-top: 5px;
+  margin-right: 5px;
+`;
+export const RightContainer = styled.div`
+  flex-grow: 1;
+  border-radius: 5px 5px 0px 0px;
+  border: 1px solid #e1e4e8;
+`;
+export const HeaderContainer = styled.div`
+  padding: 10px 15px;
+  background-color: #f6f8fa;
+  border-bottom: 1px solid #e1e4e8;
+`;
+export const DescriptContainer = styled.div`
+  margin: 20px 0px;
+  padding: 10px;
+`;
+export const TagContainer = styled.div`
+  padding: 10px;
+  border-bottom: 1px solid #e1e4e8;
+`;
 export const QusetionDetail = styled.div`
   display: flex;
+  padding: 10px;
 `;
