496a569c30ca7a045ad348c76df0da8572f329df
DevJo <sa02045@naver.com>
2021-11-11 10:35:19 +0900

    :sparkles: feat: Main-page

--- frontend/src/pages/index.tsx
+++ frontend/src/pages/index.tsx
@@ -1,11 +1,54 @@
-import type { NextPage } from "next";
+import { Header, SideBar } from "@components/organisms";
+import { QuestionList } from "@components/templates";
+import { NextPage } from "next";
+import { GetServerSideProps } from "next";
+import styled from "styled-components";
+import { QuestionData } from "../types";
+import { getQuestions } from "../lib/api";
+import { useState } from "react";
 
-const Home: NextPage = () => {
+export const MainContainer = styled.main`
+  display: flex;
+  width: 100%;
+  max-width: 1200px;
+  margin-left: auto;
+  margin-right: auto;
+`;
+interface Props {
+  data: QuestionData;
+  error: any;
+}
+
+const MainPage: NextPage<Props> = ({ data }) => {
+  const [tags, setTags] = useState([]);
+  const { searchQuestions } = data;
   return (
     <>
-      <div>메인페이지</div>
+      <Header />
+      <MainContainer>
+        <SideBar selectedTags={tags} setSelectedTags={setTags} />
+        <QuestionList questions={searchQuestions} />
+      </MainContainer>
     </>
   );
 };
 
-export default Home;
+export const getServerSideProps: GetServerSideProps = async (context) => {
+  const { data } = await getQuestions(5);
+  if (!data) {
+    return {
+      redirect: {
+        destination: "/",
+        permanent: false,
+      },
+    };
+  }
+
+  return {
+    props: {
+      data,
+    },
+  };
+};
+
+export default MainPage;
