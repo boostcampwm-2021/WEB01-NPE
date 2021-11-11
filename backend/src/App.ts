8fb4d982be5002c01aabdce72934d0a9ecd85be6
hwangwoojin <hwangwoojin.dev@gmail.com>
2021-11-11 22:10:20 +0900

    :wrench: chore: 백엔드 CORS 설정
    API 요청을 위한 임시 CORS 설정

--- backend/src/App.ts
+++ backend/src/App.ts
@@ -2,6 +2,7 @@ import "reflect-metadata";
 import express from "express";
 import GraphQLMiddleware from "./graphql";
 import { createConnection } from "typeorm";
+import cors from "cors";
 const DB_CONN_INFO = require("../ormconfig.json");
 let env = null;
 if (process.env.NODE_ENV === "production") {
@@ -16,6 +17,8 @@ if (process.env.NODE_ENV === "production") {
   createConnection(DB_CONN_INFO[env]);
 
   const gqMiddleware = await GraphQLMiddleware.get();
+
+  app.use(cors());
 
   app.use("/graphql", gqMiddleware);
 
