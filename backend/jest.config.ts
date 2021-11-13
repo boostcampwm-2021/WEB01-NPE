0c134ba9045d9334e1113f0c87e65097f7668b4b
jangdawit <jangdawit@gmail.com>
2021-11-13 22:06:44 +0900

    ✅ Chore: jest 기본 설정

---
+++ backend/jest.config.ts
@@ -0,0 +1,9 @@
+import type { Config } from "@jest/types";
+
+const config: Config.InitialOptions = {
+  preset: "ts-jest",
+  testEnvironment: "node",
+  testMatch: ["test/**/*.test.ts"],
+};
+
+export default config;
