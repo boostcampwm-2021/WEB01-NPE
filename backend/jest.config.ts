5ab63e35258634047b701ca110584ac17ef04b72
jangdawit <jangdawit@gmail.com>
2021-11-14 01:50:39 +0900

    🔧  Chore: 테스트 환경 관련 설정

--- backend/jest.config.ts
+++ backend/jest.config.ts
@@ -3,7 +3,7 @@ import type { Config } from "@jest/types";
 const config: Config.InitialOptions = {
   preset: "ts-jest",
   testEnvironment: "node",
-  testMatch: ["test/**/*.test.ts"],
+  testMatch: ["**/*.test.ts"],
 };
 
 export default config;
