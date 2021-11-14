fac7eff4bc168c24ab130e483e5b511c881bee9b
DevJo <sa02045@naver.com>
2021-11-14 19:16:52 +0900

    :recycle: Refactor: import 순서 정리, alias 적용, 옵션 분리

--- frontend/src/pages/_app.tsx
+++ frontend/src/pages/_app.tsx
@@ -1,20 +1,18 @@
-import "../styles/globals.css";
 import type { AppProps } from "next/app";
-import { ApolloProvider } from "@apollo/client";
 import { Provider as SessionProvider } from "next-auth/client";
+import { ApolloProvider } from "@apollo/client";
 
-import client from "../lib/apolloClient";
+import client from "@src/lib/apolloClient";
+import "../styles/globals.css";
 
 function MyApp({ Component, pageProps }: AppProps) {
+  const sessionOptions = {
+    clientMaxAge: 10000,
+    keepAlive: 10000,
+  };
   return (
     <ApolloProvider client={client}>
-      <SessionProvider
-        options={{
-          clientMaxAge: 10000,
-          keepAlive: 10000,
-        }}
-        session={pageProps.session}
-      >
+      <SessionProvider options={sessionOptions} session={pageProps.session}>
         <Component {...pageProps} />
       </SessionProvider>
     </ApolloProvider>
