d398b00b72c34a16ab33cbf68ebabfab5344a870
DevJo <sa02045@naver.com>
2021-11-14 21:46:07 +0900

    :fire: remove : SearchResult

--- frontend/src/components/organisms/index.ts
+++ frontend/src/components/organisms/index.ts
@@ -3,6 +3,5 @@ export { default as Header } from "./Header";
 export { default as SideBar } from "./SideBar";
 export { default as AnswerDetail } from "./AnswerDetail";
 export { default as DetailBody } from "./DetailBody";
-export { default as SearchResult } from "./SearchResult";
 export { default as QuestionDetail } from "./QuestionDetail";
 export { default as AnswerRegister } from "./AnswerRegister";
