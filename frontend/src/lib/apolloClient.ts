ee18a0f55d9b04cdd31ad90d5fddf98999996491
DevJo <sa02045@naver.com>
2021-11-15 09:04:18 +0900

    :recycle: Refactor: 개발환경에 따라 API ENDPOINT 변경

--- frontend/src/lib/apolloClient.ts
+++ frontend/src/lib/apolloClient.ts
@@ -1,7 +1,16 @@
 import { ApolloClient, InMemoryCache } from "@apollo/client";
-const client = new ApolloClient({
-  uri: `http://localhost:4000/graphql`,
-  cache: new InMemoryCache(),
-});
+
+let client;
+if (process.env.NODE_ENV === "production") {
+  client = new ApolloClient({
+    uri: `http://118.67.142.132:4000/graphql`,
+    cache: new InMemoryCache(),
+  });
+} else {
+  client = new ApolloClient({
+    uri: `http://localhost:4000/graphql`,
+    cache: new InMemoryCache(),
+  });
+}
 
 export default client;
