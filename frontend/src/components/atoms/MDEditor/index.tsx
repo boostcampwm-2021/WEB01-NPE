42e4a6a9839e4784a2406c90d735b6d57e3c1491
getState <asj446@naver.com>
2021-11-15 22:13:21 +0900

    :tada: fix: [FE] MDEditor 컴포넌트 forwardRef로 전환

--- frontend/src/components/atoms/MDEditor/index.tsx
+++ frontend/src/components/atoms/MDEditor/index.tsx
@@ -1,42 +1,11 @@
-import React, { FunctionComponent } from "react";
-import "prismjs/themes/prism.css";
-import Prism from "prismjs";
-import "@toast-ui/editor/dist/toastui-editor.css";
-// import codeSyntaxHighlight from "@toast-ui/editor-plugin-code-syntax-highlight";
-import { EditorProps } from "@toast-ui/react-editor";
+import React, { FunctionComponent, useRef } from "react";
 import dynamic from "next/dynamic";
-const Editor = dynamic<EditorProps>(
-  () => import("@toast-ui/react-editor").then((m) => m.Editor),
-  { ssr: false }
-);
-// const codeSyntaxHighlight = dynamic(
-//   () => import("@toast-ui/editor-plugin-code-syntax-highlight").then((m) => m),
-//   { ssr: false }
-// );
-// const colorSyntax = dynamic(
-//   () => import("@toast-ui/editor-plugin-color-syntax").then((m) => m.default),
-//   { ssr: false }
-// );
 interface Props {
-  editorRef: any;
   type?: string;
 }
+const WrappedEditor = dynamic(() => import("./WrappedEditor"), { ssr: false });
 
-const MDEditor: FunctionComponent<Props> = ({ type, editorRef }) => {
-  return (
-    <Editor
-      ref={editorRef}
-      initialValue={
-        type === "Answer" ? `## 답변을 입력해주세요` : `## 질문을 입력해주세요.`
-      }
-      previewStyle="tab"
-      height={type === "Answer" ? "300px" : "600px"}
-      initialEditType="markdown"
-      useCommandShortcut={true}
-      // plugins={[colorSyntax, [codeSyntaxHighlight, { highlighter: Prism }]]}
-      // plugins={[codeSyntaxHighlight, { highlighter: Prism }]}
-    />
-  );
-};
-
+const MDEditor = React.forwardRef<any, Props>((props, ref) => {
+  return <WrappedEditor type={props.type} editorRef={ref} />;
+});
 export default MDEditor;
