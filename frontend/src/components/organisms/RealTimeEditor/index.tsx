4822b4f8c20581b0fd4f5b19e873c1d9f46e5b37
hwangwoojin <hwangwoojin.dev@gmail.com>
2021-11-18 00:29:44 +0900

    :art: feat: [FE] RealTimeEditor 구현
    실시간 코드 공유를 위한 RealTimeEditor 컴포넌트 구현

---
+++ frontend/src/components/organisms/RealTimeEditor/index.tsx
@@ -0,0 +1,9 @@
+import React, { FunctionComponent } from "react";
+
+import * as Styled from "./styled";
+
+const RealTimeEditor: FunctionComponent = () => {
+  return <Styled.Editor>파일</Styled.Editor>;
+};
+
+export default RealTimeEditor;
