bf02bf5ff3a12f3c8601948dde505f3df612f6a4
jangdawit <jangdawit@gmail.com>
2021-11-18 22:25:55 +0900

    :zap: Feature: 실시간 모달 나가기 모달 및 질문자 답변자 퇴장 분기처리

--- backend/src/socket/index.ts
+++ backend/src/socket/index.ts
@@ -11,38 +11,60 @@ interface SessionUser {
   expires: string;
 }
 
-const roomUsers: Record<string, object[]> = {};
+// roomId to user Object
+const roomUsers: Record<string, SessionUser[]> = {};
+// userId to roomId
+const usersRoom: Record<string, string> = {};
 
 export default (io: socketio.Server) => {
   io.use((socket, next) => {
     const token = socket.handshake.auth.token;
     const user = verify(token, "keyboard cat") as SessionUser;
+    if (!user) return;
     next();
   });
 
   io.on("connection", (socket) => {
-    console.log(socket.id + " is connected");
+    //console.log(socket.id + " is connected");
 
     socket.on("joinRoom", ({ questionId }: { questionId: string }) => {
       const token = socket.handshake.auth.token;
       const user = verify(token, "keyboard cat") as SessionUser;
 
+      const roomName = questionId;
       // 4명 미만인지 확인. 4명 이상이면 disconnect
-      if (!roomUsers[questionId]) {
-        roomUsers[questionId] = [user];
-      } else if (roomUsers[questionId].length >= 4) {
+      if (!roomUsers[roomName]) {
+        roomUsers[roomName] = [user];
+        usersRoom[user.userId] = roomName;
+      } else if (roomUsers[roomName].length >= 4) {
         socket.disconnect();
         return;
       } else {
-        roomUsers[questionId].push(user);
+        roomUsers[roomName].push(user);
+        usersRoom[user.userId] = roomName;
       }
 
-      const roomName = questionId;
       socket.join(roomName);
+      console.log(
+        `${user.user.name} joining ${roomName} -> ${roomUsers[roomName].length}/4`
+      );
     });
 
     socket.on("disconnect", (reason) => {
-      console.log(socket.id + " is disconnected. reason : " + reason);
+      //console.log(socket.id + " is disconnected. reason : " + reason);
+
+      const token = socket.handshake.auth.token;
+      const user = verify(token, "keyboard cat") as SessionUser;
+      const userId = user.userId;
+      const roomName = usersRoom[userId];
+
+      roomUsers[roomName] = roomUsers[roomName].filter(
+        (user) => user.userId !== userId
+      );
+      delete usersRoom[userId];
+      console.log(
+        `${user.user.name} leaving ${roomName} -> ${roomUsers[roomName].length}/4`
+      );
     });
   });
 };
--- frontend/src/components/templates/RealTimeModal/index.tsx
+++ frontend/src/components/templates/RealTimeModal/index.tsx
@@ -1,33 +1,45 @@
-import React, { FunctionComponent } from "react";
+import React, { FunctionComponent, useState } from "react";
 import * as Socket from "socket.io-client";
 
 import * as Styled from "./styled";
-import { Button, Logo, TitleText, Text } from "@components/atoms";
+import { Button, Logo, TitleText, Text, ContentText } from "@components/atoms";
 import { RealTimeEditor } from "@components/organisms";
 import { QuestionDetailType } from "@src/types";
 import { useSession } from "next-auth/client";
+import ExitCheckModalWarpper from "@src/components/molecules/ExitCheckModalWapper";
 
-const RealTimeModal: FunctionComponent<{ question: QuestionDetailType }> = ({
-  question,
-}) => {
-  let socket: Socket.Socket;
+const RealTimeModal: FunctionComponent<{
+  question: QuestionDetailType;
+  exitModal: VoidFunction;
+}> = ({ question, exitModal }) => {
+  const [SOCKET, setSOCKET] = useState<Socket.Socket | null>(null);
   const [session] = useSession();
   if (!session || !session.accessToken) throw new Error("Auth Required");
 
   React.useEffect(() => {
-    const token = session.accessToken;
-    socket = Socket.connect("http://localhost:4000/", {
-      auth: { token },
-    });
-    socket.emit("joinRoom", {
-      questionId: question.id,
-    });
-    return () => {
-      socket.disconnect();
-    };
+    if (!SOCKET) {
+      const token = session.accessToken;
+      const socket = Socket.connect("http://localhost:4000/", {
+        auth: { token },
+      });
+      socket.emit("joinRoom", {
+        questionId: question.id,
+      });
+      setSOCKET(socket);
+    }
   });
 
   const yjssocket = "?";
+
+  const onExitClick = () => {
+    const exitModalDiv = document.getElementById("exitModal");
+    exitModalDiv!.style.visibility = "visible";
+  };
+
+  const disconnectAndExit = () => {
+    SOCKET?.disconnect();
+    exitModal();
+  };
 
   return (
     <Styled.ModalWrapper>
@@ -37,6 +49,15 @@ const RealTimeModal: FunctionComponent<{ question: QuestionDetailType }> = ({
           <RealTimeEditor question={question} />
           <div>채팅</div>
         </Styled.temp>
+        <Button
+          type="realtime_exit"
+          text="나가기"
+          onClick={onExitClick}
+        ></Button>
+        <ExitCheckModalWarpper
+          question={question}
+          disconnectAndExit={disconnectAndExit}
+        />
       </Styled.Modal>
     </Styled.ModalWrapper>
   );
