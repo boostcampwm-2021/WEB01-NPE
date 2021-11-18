64504158d2e6fc5da6703d5f6aff0437fa573ad5
jangdawit <jangdawit@gmail.com>
2021-11-18 10:47:35 +0900

    :zap: Feature: 방 인원 확인/제한 로직

--- backend/src/socket/index.ts
+++ backend/src/socket/index.ts
@@ -11,6 +11,8 @@ interface SessionUser {
   expires: string;
 }
 
+const roomUsers: Record<string, object[]> = {};
+
 export default (io: socketio.Server) => {
   io.use((socket, next) => {
     const token = socket.handshake.auth.token;
@@ -24,7 +26,17 @@ export default (io: socketio.Server) => {
     socket.on("joinRoom", ({ questionId }: { questionId: string }) => {
       const token = socket.handshake.auth.token;
       const user = verify(token, "keyboard cat") as SessionUser;
-      console.log(user);
+
+      // 4명 미만인지 확인. 4명 이상이면 disconnect
+      if (!roomUsers[questionId]) {
+        roomUsers[questionId] = [user];
+      } else if (roomUsers[questionId].length >= 4) {
+        socket.disconnect();
+        return;
+      } else {
+        roomUsers[questionId].push(user);
+      }
+
       const roomName = questionId;
       socket.join(roomName);
     });
