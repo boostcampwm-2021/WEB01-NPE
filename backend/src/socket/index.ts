ae98037a85f772d1f4997383f6442b78c49a5e68
getState <asj446@naver.com>
2021-11-18 19:17:38 +0900

    :zap: feat: [BE] socket에서 chat과 user 관련 기능 구현

--- backend/src/socket/index.ts
+++ backend/src/socket/index.ts
@@ -10,6 +10,13 @@ interface SessionUser {
   };
   expires: string;
 }
+interface UsersType {
+  [roomName: string]: {
+    [socketId: string]: SessionUser;
+  };
+}
+
+const users: UsersType = {};
 
 export default (io: socketio.Server) => {
   io.use((socket, next) => {
@@ -21,14 +28,28 @@ export default (io: socketio.Server) => {
 
   io.on("connection", (socket) => {
     console.log(socket.id + " is connected");
-
+    let roomName = "";
     socket.on("joinRoom", ({ questionId }: { questionId: string }) => {
-      const roomName = questionId;
+      roomName = questionId;
       socket.join(roomName);
+      const token = socket.handshake.auth.token;
+      const user = verify(token, "keyboard cat") as SessionUser;
+      if (users[roomName] === undefined) {
+        users[roomName] = {};
+      }
+      users[roomName][socket.id] = user;
+      socket.emit("init users", users[roomName]);
+      socket.to(roomName).emit("user join", [socket.id, user]);
+    });
+    socket.on("disconnect", (reason) => {
+      // delete users[roomName][socket!.id];
+      console.log(socket.id + " is disconnected. reason : " + reason);
     });
 
-    socket.on("disconnect", (reason) => {
-      console.log(socket.id + " is disconnected. reason : " + reason);
+    socket.on("chat", (chatItem) => {
+      console.log(roomName);
+      console.log(roomName, chatItem);
+      io.to(roomName).emit("chat", chatItem);
     });
   });
 };
