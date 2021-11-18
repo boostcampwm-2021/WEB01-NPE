4e12bf2f7edcec27f8eaa31375447e12bff96ec2
hwangwoojin <hwangwoojin.dev@gmail.com>
2021-11-19 07:59:52 +0900

    :sparkles: feat: [FE] 코드 공유를 질문/답변 분리
    질문 답변 분리 후 클릭해서 이동할 수 있도록 함

--- frontend/src/components/organisms/RealTimeEditor/WrappedEditor.tsx
+++ frontend/src/components/organisms/RealTimeEditor/WrappedEditor.tsx
@@ -1,86 +1,57 @@
-import React, { FunctionComponent, useEffect, useState } from "react";
-import * as Y from "yjs";
-import { WebsocketProvider } from "y-websocket";
-import { CodemirrorBinding } from "y-codemirror";
-import { UnControlled as CodeMirror } from "react-codemirror2";
+import React, { FunctionComponent, MouseEvent, useState } from "react";
 import "codemirror/theme/material.css";
 import "codemirror/lib/codemirror.css";
 import "codemirror/mode/gfm/gfm";
-import { useSession } from "next-auth/client";
 import RandomColor from "randomcolor";
 
 import * as Styled from "./styled";
+import Editor from "./editor";
 import { QuestionDetailType } from "@src/types";
 
 const WrappedEditor: FunctionComponent<{
   question: QuestionDetailType;
 }> = ({ question }) => {
-  const [editorRef, setEditorRef] = useState(null);
-  const [session] = useSession();
+  const [currentEditor, setCurrentEditor] = useState("question");
 
   const color = RandomColor();
 
-  const onEditorDidMount = (editor: CodeMirror.Editor) => {
-    setEditorRef(editor);
+  const onTabClick = (target: string) => () => {
+    setCurrentEditor(target);
   };
-
-  const setDefaultVal = (ytext: Y.Text) => {
-    if (ytext.toString() === "") {
-      ytext.insert(0, question.desc);
-    }
-  };
-
-  useEffect(() => {
-    if (!editorRef) return;
-
-    const ydoc = new Y.Doc();
-    const roomId = `${question.id}`;
-    const provider = new WebsocketProvider("ws://localhost:1234", roomId, ydoc);
-    const yText = ydoc.getText("codemirror");
-    const yUndoManager = new Y.UndoManager(yText);
-    provider.awareness.setLocalStateField("user", {
-      name: session?.user?.name,
-      color: color,
-    });
-    if (provider.synced) {
-      setDefaultVal(yText);
-    } else {
-      provider.once("synced", () => setDefaultVal(yText));
-    }
-    const binding = new CodemirrorBinding(
-      yText,
-      editorRef!,
-      provider.awareness,
-      {
-        yUndoManager,
-      }
-    );
-
-    return () => {
-      if (provider) {
-        provider.disconnect();
-        ydoc.destroy();
-      }
-    };
-  }, [editorRef]);
 
   return (
     <Styled.Editor>
       <Styled.Tab>
-        <Styled.Code>버튼1</Styled.Code>
-        <Styled.Code>버튼1</Styled.Code>
-        <Styled.Code>버튼1</Styled.Code>
-        <Styled.Code>버튼1</Styled.Code>
+        <Styled.Code
+          focused={currentEditor === "question"}
+          onClick={onTabClick("question")}
+        >
+          질문
+        </Styled.Code>
+        <Styled.Code
+          focused={currentEditor === "answer"}
+          onClick={onTabClick("answer")}
+        >
+          답변
+        </Styled.Code>
       </Styled.Tab>
-      <CodeMirror
-        options={{
-          mode: "gfm",
-          theme: "material",
-          lineNumbers: true,
-        }}
-        onChange={(editor, data, value) => {}}
-        editorDidMount={onEditorDidMount}
-      />
+      <div>
+        {currentEditor === "question" && (
+          <Editor
+            question={question}
+            roomId={`${question.id}-question`}
+            color={color}
+            value={question.desc}
+          />
+        )}
+        {currentEditor === "answer" && (
+          <Editor
+            question={question}
+            roomId={`${question.id}-answer`}
+            color={color}
+          />
+        )}
+      </div>
     </Styled.Editor>
   );
 };
--- frontend/src/components/organisms/RealTimeEditor/styled.ts
+++ frontend/src/components/organisms/RealTimeEditor/styled.ts
@@ -38,15 +38,20 @@ export const Editor = styled.div`
 
 export const Tab = styled.div`
   display: flex;
-  background-color: rgba(0, 0, 0, 0.3);
-  height: 50px;
+  background-color: rgba(0, 0, 0, 0.1);
+  height: 40px;
   border-bottom: 1px solid;
 `;
 
-export const Code = styled.div`
+export const Code = styled.div<{ focused: boolean }>`
   display: flex;
   justify-content: center;
   align-items: center;
   padding: 0px 20px 0px 20px;
   border-right: 1px solid;
+  ${(props) => props.focused && "background-color: rgba(0, 0, 0, 0.2);"}
+
+  :hover {
+    cursor: pointer;
+  }
 `;
