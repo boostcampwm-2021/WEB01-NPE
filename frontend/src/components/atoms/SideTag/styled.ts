90b7905a13d3241a3dd1792999445790796a7a41
getState <asj446@naver.com>
2021-11-23 21:49:27 +0900

    :construction: fix: [FE] SideTag ATom 스타일 수정

--- frontend/src/components/atoms/SideTag/styled.ts
+++ frontend/src/components/atoms/SideTag/styled.ts
@@ -11,25 +11,25 @@ interface DeleteButtonProps {
 export const TagContainer = styled.li<TagProps>`
   display: flex;
   justify-content: space-between;
-  width: 150px;
-  background-color: ${(props) => props.tagBgColor};
+  // background-color: ${(props) => props.tagBgColor};
+  background-color: #2d3e4f;
   border-radius: 50px;
   color: ${(props) => props.textColor};
   box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.1), 0px 4px 20px rgba(0, 0, 0, 0.1);
-  margin-bottom: 15px;
+  margin: 0px 0px 10px 7px;
 `;
 
 export const TagText = styled.div`
-  padding: 5px 0px 8px 15px;
+  padding: 3px 7px 6px 10px;
 `;
 
 export const DeleteButton = styled.button<DeleteButtonProps>`
-  width: 30px;
   border-radius: 0px 50px 50px 0px;
   color: white;
   border: none;
-  background-color: ${(props) => props.deleteBgColor};
+  background-color: #2d3e4f;
   cursor: pointer;
+  padding-left: 0px;
   &:hover {
     box-shadow: 0 5px 15px rgba(0, 0, 0, 0.3);
   }
