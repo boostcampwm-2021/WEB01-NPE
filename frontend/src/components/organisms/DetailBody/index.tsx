dea63164727d99dbadbb4f71e1dc97babebe728b
DevJo <sa02045@naver.com>
2021-11-24 23:47:35 +0900

    feat: 답변삭제

--- frontend/src/components/organisms/DetailBody/index.tsx
+++ frontend/src/components/organisms/DetailBody/index.tsx
@@ -1,17 +1,28 @@
 import React, { FunctionComponent } from "react";
+import Router from "next/router";
 
 import * as Styled from "./styled";
 import { TagList, ProfileSummary, Vote } from "@components/molecules";
 import { MDViewer } from "@components/atoms";
 import { DetailType } from "@src/types";
-
+import { useSession } from "next-auth/client";
+import { deleteAnswerById } from "@src/lib";
 interface Props {
   detail: DetailType;
-  type: "Answer" | "Question";
+  type: "Question" | "Answer";
 }
 
-const DetailBody: FunctionComponent<Props> = ({ detail }) => {
+const DetailBody: FunctionComponent<Props> = ({ detail, type }) => {
   const { id, thumbupCount, desc, tags, author } = detail;
+  const user = useSession();
+  const onDelete = async () => {
+    if (window.confirm("삭제하시겠습니까?")) {
+      const { data } = await deleteAnswerById(Number(id));
+      if (data) {
+        Router.push("/");
+      }
+    }
+  };
   return (
     <Styled.DetailBody>
       <Styled.VoteContainer>
@@ -26,6 +37,10 @@ const DetailBody: FunctionComponent<Props> = ({ detail }) => {
           <MDViewer content={desc} />
         </Styled.DetailBodyDesc>
         <Styled.DetailBodyInfo>
+          {type === "Answer" &&
+            Number(user[0]?.userId) === Number(author.id) && (
+              <Styled.DeleteBtn onClick={onDelete}>삭제</Styled.DeleteBtn>
+            )}
           <div>{tags && <TagList tags={tags} />}</div>
           <div>
             <ProfileSummary author={author} />
