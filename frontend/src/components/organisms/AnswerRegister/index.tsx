d94dfd9848146bc41d237a496d9f210fa4a840d9
DevJo <sa02045@naver.com>
2021-11-24 11:48:00 +0900

    :bug: fix: question id Scorll
    window.scrollTo로 강제로 위로 고정, 깜빡임 이슈

--- frontend/src/components/organisms/AnswerRegister/index.tsx
+++ frontend/src/components/organisms/AnswerRegister/index.tsx
@@ -1,12 +1,5 @@
-import { gql, useMutation } from "@apollo/client";
-import { useRouter } from "next/router";
-import React, {
-  FormEvent,
-  FunctionComponent,
-  useEffect,
-  useRef,
-  useState,
-} from "react";
+import { useMutation } from "@apollo/client";
+import React, { FunctionComponent, useEffect, useRef, useState } from "react";
 import { useSession } from "next-auth/client";
 
 import { MDEditor, Button } from "@components/atoms";
@@ -58,10 +51,16 @@ const AnswerRegister: FunctionComponent<Props> = ({
     editorInstance.setHTML(value);
   }, [value]);
 
+  useEffect(() => {
+    setTimeout(() => {
+      window.scrollTo(0, 0);
+    });
+  }, []);
+
   return (
     <Styled.AnswerRegister onSubmit={onSubmit}>
       <h2>당신의 답변</h2>
-      {<MDEditor type="Answer" ref={editorRef} />}
+      <MDEditor type="Answer" ref={editorRef} />
       <Styled.AnswerBtnContainer>
         <Button type="Submit" text="답변하기" onClick={() => {}} />
       </Styled.AnswerBtnContainer>
