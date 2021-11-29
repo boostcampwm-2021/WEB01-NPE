a34fcaa5cdfdba7395f91bfb9d47596a7a03f006
getState <asj446@naver.com>
2021-11-29 20:01:48 +0900

    :zap: fix: [FE] 메인 페이지 질문리스트 가로 너비 고정

--- frontend/src/components/templates/QuestionList/styled.ts
+++ frontend/src/components/templates/QuestionList/styled.ts
@@ -3,6 +3,7 @@ import styled from "styled-components";
 export const QuestionList = styled.ul`
   display: flex;
   flex-direction: column;
+  width: 640px;
 `;
 
 export const QuestionItem = styled.li``;
