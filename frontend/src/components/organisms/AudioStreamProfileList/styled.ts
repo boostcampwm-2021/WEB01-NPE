493a95bc7d7d7fad8b5745d3ab0a18381157101d
DevJo <sa02045@naver.com>
2021-12-06 00:36:41 +0900

    :lipstick: style: AudioStreamProfileList

--- frontend/src/components/organisms/AudioStreamProfileList/styled.ts
+++ frontend/src/components/organisms/AudioStreamProfileList/styled.ts
@@ -3,8 +3,6 @@ import styled from "styled-components";
 export const ProfileList = styled.ul`
   display: flex;
   flex-direction: column;
-  margin-left: 25px;
-  margin-top: 25px;
   div {
     display: flex;
     justify-content: space-between;
