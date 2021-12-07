f5f429f9a9b6e28703fab7e6c472d4f6c5b40b68
jangdawit <jangdawit@gmail.com>
2021-12-07 20:58:33 +0900

    ✅ Chore: 단위 테스트, 통합 테스트 분리

--- backend/test/integration/service/UserService.test.ts
+++ backend/test/integration/service/UserService.test.ts
@@ -1,9 +1,7 @@
 import Container from "typedi";
-import UserService from "../../src/services/User/UserService";
-import faker from "faker";
-import { User } from "../../src/entities/User";
+import UserService from "../../../src/services/User/UserService";
 import connection from "../connection";
-import InjectionConfig from "../../src/InjectionConfig";
+import InjectionConfig from "../../../src/InjectionConfig";
 import { TransactionalTestContext } from "typeorm-transactional-tests";
 import { Connection, EntityManager } from "typeorm";
 import UserMock from "../mockdata/userMock";
